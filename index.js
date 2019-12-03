5a5072edbbecd47a1a439a2b8bab46422169c98a
Eddie Hernandez <mchh.004@gmail.com>
2019-12-03 04:04:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function dwarfRollCall(dwarves) {
+var dwarves = [ "Dopey", "Grumpy", "Bashful", "Doc" ]
+  function dwarfRollCall(dwarves){
+    var rollCall = ""
+    dwarves.forEach(function(name,i) {
+        rollCall += (i+1 + ". " + name + " ")
+    })
+  return rollCall
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+  function summonCaptainPlanet(planeteerCalls){
+    var yell = []
+    planeteerCalls.forEach(function(element){
+      element = element.toUpperCase() + "!"
+      yell.push(element)
+    })
+  return yell
 }
 
-function longPlaneteerCalls(words) {
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+function longPlaneteerCalls(planeteerCalls) {
+  for (var i = 0; i < planeteerCalls.length; i++) {
+  if (planeteerCalls.length > 4) {
+    return true
+  }
+    else {
+      return false
+    }
+  }
 }
 
+
+var foods = [ "crackers", "gouda", "thyme", "tomato soup", "cheddar", "oyster crackers", "gouda", "garlic", "rosemary", "bread" ]
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+    if ( foods[i] === "gouda" || foods[i] === "cheddar" || foods[i] === "camembert" ) {
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
